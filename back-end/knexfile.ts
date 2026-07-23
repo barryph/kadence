36f3eb93c025c58760fc78605d8fb487cb60d064
Barry Hood <barryph@protonmail.com>
2026-07-23 18:48:47 +1200

    feat: configure knex db connection for prod

--- back-end/knexfile.ts
+++ back-end/knexfile.ts
@@ -1,16 +1,46 @@
 import 'dotenv/config';
 import path from 'path';
 import type { Knex } from 'knex';
+import * as fs from 'node:fs';
 
+const isProduction = process.env.NODE_ENV === 'production';
 const root = path.resolve(process.cwd());
 
 export const development: Knex.Config = {
   client: 'pg',
   connection: {
-    host: '127.0.0.1',
+    host: process.env.DATABASE_HOST,
     user: process.env.POSTGRES_USER,
     password: process.env.POSTGRES_PASSWORD,
     database: process.env.DATABASE_NAME,
+  },
+  migrations: {
+    directory: path.join(root, '/src/shared/knex/migrations'),
+  },
+  seeds: {
+    directory: path.join(root, '/src/shared/knex/seeds'),
+  },
+};
+
+export const production: Knex.Config = {
+  client: 'pg',
+  connection: {
+    host: process.env.DATABASE_HOST,
+    user: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.DATABASE_NAME,
+    // global-bundle.pem does not exist outside of prod,
+    // we check isProduction to avoid erroring attempting to read a file that doesn't exist
+    ...(isProduction && {
+      ssl: {
+        ca: fs.readFileSync('global-bundle.pem'),
+        rejectUnauthorized: false, // Set to false if using self-signed certificates
+      },
+    }),
+  },
+  pool: {
+    min: 2,
+    max: 10,
   },
   migrations: {
     directory: path.join(root, '/src/shared/knex/migrations'),
@@ -35,19 +65,3 @@ export const development: Knex.Config = {
 //     tableName: "knex_migrations"
 //   }
 // },
-
-// production: {
-//   client: "postgresql",
-//   connection: {
-//     database: "my_db",
-//     user: "username",
-//     password: "password"
-//   },
-//   pool: {
-//     min: 2,
-//     max: 10
-//   },
-//   migrations: {
-//     tableName: "knex_migrations"
-//   }
-// }
--- back-end/src/shared/knex/knex.service.ts
+++ back-end/src/shared/knex/knex.service.ts
@@ -2,12 +2,14 @@ import { Injectable } from '@nestjs/common';
 import knex from 'knex';
 import * as config from '../../../knexfile';
 
+const environment = process.env.NODE_ENV || 'development';
+
 @Injectable()
 export class KnexService {
   private db: knex.Knex;
 
   constructor() {
-    this.db = knex(config.development);
+    this.db = knex(config[environment]);
   }
 
   get connection() {

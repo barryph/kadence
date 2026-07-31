f24e45054ec327ecfd47629cccd6970d282222b1
Barry Hood <barryph@protonmail.com>
2026-07-31 12:23:22 +1200

    Disable swagger in production

--- back-end/src/main.ts
+++ back-end/src/main.ts
@@ -19,8 +19,10 @@ async function bootstrap() {
     .setDescription('The habits API description')
     .setVersion('1.0')
     .build();
-  const documentFactory = () => SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, documentFactory);
+  if (process.env.NODE_ENV !== 'production') {
+    const documentFactory = () => SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('api', app, documentFactory);
+  }
 
   await app.listen(process.env.PORT ?? 3000, '0.0.0.0');
 }

8005612144ac6a6bffa4e236bcc8f69a01eac309
Barry Hood <barryph@protonmail.com>
2026-07-31 12:51:47 +1200

    clean: remove old proxy tunnel variables

--- front-end/api/api.client.ts
+++ front-end/api/api.client.ts
@@ -5,9 +5,6 @@ export interface OptionalOptions {
   signal?: AbortSignal;
 }
 
-// const BASE_URL = 'https://yummy-cooks-train.loca.lt';
-// const BASE_URL = Platform.OS === 'android' ? 'http://10.0.2.2:3000' : 'http://localhost:3000';
-// const BASE_URL = Platform.OS === 'android' ? 'https://9d68-203-211-79-9.ngrok-free.app' : 'http://localhost:3000';
 const BASE_URL = process.env.EXPO_PUBLIC_SERVER_URL;
 
 class APIClient {
@@ -24,7 +21,6 @@ class APIClient {
         credentials: 'include',
         headers: new Headers({
           'Content-Type': 'application/json',
-          'bypass-tunnel-reminder': 'true',
           ...options.headers,
         }),
       });

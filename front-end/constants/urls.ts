e4fc55587ad0de359fe909f342b19d500dd7374d
Barry Hood <barryph@protonmail.com>
2026-09-19 16:34:43 +1200

    fix(privacypolicy): set the correct privacy policy url

--- front-end/constants/urls.ts
+++ front-end/constants/urls.ts
@@ -5,4 +5,4 @@
  * places and updates only have to happen once.
  */
 
-export const PRIVACY_POLICY_URL = 'https://kadence.barryph.com/privacy-policy';
+export const PRIVACY_POLICY_URL = `${process.env.EXPO_PUBLIC_PRIVACY_POLICY_SITE_URL}/privacy-policy`;
